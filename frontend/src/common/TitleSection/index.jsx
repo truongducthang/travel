c625dec29476c59335a78c7c042ac2d21c68c1a2
truongducthang <ducthangm9@gmail.com>
2021-04-20 23:07:36 +0700

    TITLE

---
+++ frontend/src/common/TitleSection/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TitleSection = () => {
+    return (
+        <div>
+                    <h1 className="h1">
+          OUR <span>DESTINATIONS</span>
+        </h1>
+        <h3 className="h3">CHOOSE YOUR NEXT DESTINATION</h3>
+        </div>
+    );
+};
+
+export default TitleSection;
