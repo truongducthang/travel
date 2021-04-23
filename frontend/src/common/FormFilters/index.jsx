d70ba2ee0908171cf461fff12faabb1b7b40f8c8
truongducthang <ducthangm9@gmail.com>
2021-04-24 03:12:58 +0700

    FormFilters

--- frontend/src/common/FormFilters/index.jsx
+++ frontend/src/common/FormFilters/index.jsx
@@ -1,36 +1,62 @@
-import React from 'react';
-import { Col, Row } from 'antd';
+import { Col, DatePicker, InputNumber, Row, Select, Slider } from 'antd';
+import moment from 'moment';
+import React, { useState } from 'react';
 import './_FormFilters.scss';
-import { Select } from 'antd';
-
+import { Checkbox } from 'antd';
+const { RangePicker } = DatePicker;
 const { Option } = Select;
 const FormFilters = () => {
-  function onChange(value) {
+  const [price, setPrice] = useState({ min: 0, max: 999 });
+  function onChangeDestination(value) {
     console.log(`selected ${value}`);
   }
 
-  function onBlur() {
+  function onBlurDestination() {
     console.log('blur');
   }
 
-  function onFocus() {
+  function onFocusDestination() {
     console.log('focus');
   }
 
-  function onSearch(val) {
+  function onSearchDestination(val) {
     console.log('search:', val);
+  }
+  function onChangeDate(dates, dateStrings) {
+    console.log('From: ', dates[0], ', to: ', dates[1]);
+    console.log('From: ', dateStrings[0], ', to: ', dateStrings[1]);
+  }
+  function onChangeMinPrice(value) {
+    console.log('changed', value);
+    setPrice((prevPrice) => {
+      return { ...prevPrice, min: value };
+    });
+  }
+  function onChangeMaxPrice(value) {
+    setPrice((prevPrice) => {
+      return { ...prevPrice, max: value };
+    });
+  }
+  function onChangePrice(value) {
+    console.log('onChange: ', value);
+    console.log('h:', price.min);
+    setPrice({ ...price, min: value[0], max: value[1] });
+  }
+  function onChangeCheckOnlyPromotions(e) {
+    console.log(`checked = ${e.target.checked}`);
   }
   return (
     <div className="form-filters">
       <Row gutter={[24, 24]}>
-        <Col span={8} xs={8} sm={8} md={8}>
+        <Col span={8} xs={24} sm={24} md={24} lg={8}>
           <div className="form-filters__col">
             <div className="form-filters__title">Select Your Destination :</div>
             <Select
-              onChange={onChange}
-              onFocus={onFocus}
-              onBlur={onBlur}
-              onSearch={onSearch}
+              className="form-filters__input"
+              onChange={onChangeDestination}
+              onFocus={onFocusDestination}
+              onBlur={onBlurDestination}
+              onSearch={onSearchDestination}
               showSearch
               style={{ width: 200 }}
               placeholder="Search to Select "
@@ -51,17 +77,70 @@ const FormFilters = () => {
               <Option value="5">Resolved</Option>
               <Option value="6">Cancelled</Option>
             </Select>
-            ,
           </div>
         </Col>
-        <Col span={8} xs={8} sm={8} md={8}>
+        <Col span={8} xs={24} sm={24} md={24} lg={8}>
           <div className="form-filters__col">
-            <div className="form-filters__title">Select Your Date :</div>
+            <div className="form-filters__title">{`Check-in & Check-out Date :`}</div>
+            <RangePicker
+              className="form-filters__input"
+              ranges={{
+                Today: [moment(), moment()],
+                'This Month': [
+                  moment().startOf('month'),
+                  moment().endOf('month'),
+                ],
+              }}
+              onChange={onChangeDate}
+            />
           </div>
         </Col>
-        <Col span={8} xs={8} sm={8} md={8}>
+        <Col span={8} xs={24} sm={24} md={24} lg={8}>
           <div className="form-filters__col">
-            <div className="form-filters__title">Max Price :</div>
+            <div style={{ display: 'flex' }}>
+              <div
+                className="form-filters__title"
+                style={{ marginBottom: '0' }}
+              >
+                Price :
+              </div>
+              <div className="form-filters__input-price-wrap">
+                <InputNumber
+                  className="form-filters__input-price"
+                  value={price.min}
+                  formatter={(value) =>
+                    `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                  }
+                  parser={(value) => value.replace(/\$\s?|(,*)/g, '')}
+                  min={0}
+                  max={999}
+                  onChange={onChangeMinPrice}
+                />
+                <span> - </span>
+                <InputNumber
+                  className="form-filters__input-price"
+                  value={price.max}
+                  formatter={(value) =>
+                    `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                  }
+                  parser={(value) => value.replace(/\$\s?|(,*)/g, '')}
+                  onChange={onChangeMaxPrice}
+                  min={0}
+                  max={999}
+                />
+              </div>
+            </div>
+            <Slider
+              range
+              value={[price.min, price.max]}
+              onChange={onChangePrice}
+              min={0}
+              max={999}
+              style={{ margin: '1rem .5rem 1.5rem' }}
+            />
+            <Checkbox onChange={onChangeCheckOnlyPromotions}>
+              <label className="form-filters__label">See only promotions</label>
+            </Checkbox>
           </div>
         </Col>
       </Row>
--- frontend/src/common/HeaderImageLayout/index.jsx
+++ frontend/src/common/HeaderImageLayout/index.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import TitleSection from '../TitleSection';
-import './_HeaderImageLayout.scss';
-import { Menu, Dropdown, message } from 'antd';
 import { DownOutlined } from '@ant-design/icons';
+import { Dropdown, Menu, message } from 'antd';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import FormFilters from '../FormFilters';
+import TitleSection from '../TitleSection';
+import './_HeaderImageLayout.scss';
 
 const HeaderImageLayout = () => {
   const onClickFiltersPrice = ({ key }) => {
@@ -29,41 +29,68 @@ const HeaderImageLayout = () => {
     </Menu>
   );
   return (
-    <div className="HeaderImageLayout">
-      <div className="HeaderImageLayout__content">
-        <div className="HeaderImageLayout__title">
-          <TitleSection
-            color="#fff"
-            fontWeight="700"
-            fontSize="3.4rem"
-            colorUnderline="#e27557"
-            hideSuggest={true}
-            title="Search your"
-            title_ul="Holiday"
-          />
-        </div>
-        <FormFilters />
-        <div className="HeaderImageLayout__filters-wrap">
-          <Dropdown overlay={menuPrice}>
-            <Link
-              to=""
-              className="ant-dropdown-link HeaderImageLayout__filters HeaderImageLayout__filtersPrice "
-              onClick={(e) => e.preventDefault()}
-            >
-              Price <DownOutlined />
-            </Link>
-          </Dropdown>
-          <Dropdown overlay={menuName}>
-            <Link
-              to=""
-              className="ant-dropdown-link  HeaderImageLayout__filters HeaderImageLayout__filtersName"
-              onClick={(e) => e.preventDefault()}
-            >
-              Name <DownOutlined />
-            </Link>
-          </Dropdown>
+    <div>
+      <div className="HeaderImageLayout">
+        <div className="HeaderImageLayout__content">
+          <div className="HeaderImageLayout__title">
+            <TitleSection
+              color="#fff"
+              fontWeight="700"
+              fontSize="3.4rem"
+              colorUnderline="#e27557"
+              hideSuggest={true}
+              title="Search your"
+              title_ul="Holiday"
+            />
+          </div>
+          <div className="FormFilters__top">
+            <FormFilters />
+          </div>
+          <div className="HeaderImageLayout__filters-wrap">
+            <Dropdown overlay={menuPrice}>
+              <Link
+                to=""
+                className="ant-dropdown-link HeaderImageLayout__filters HeaderImageLayout__filtersPrice "
+                onClick={(e) => e.preventDefault()}
+              >
+                Price <DownOutlined />
+              </Link>
+            </Dropdown>
+            <Dropdown overlay={menuName}>
+              <Link
+                to=""
+                className="ant-dropdown-link  HeaderImageLayout__filters HeaderImageLayout__filtersName"
+                onClick={(e) => e.preventDefault()}
+              >
+                Name <DownOutlined />
+              </Link>
+            </Dropdown>
+          </div>
         </div>
       </div>
+      {/* screen >= lg - none  */}
+      <div className="FormFilters__bottom">
+        <FormFilters />
+      </div>
+      {/* end- screen lg   */}
+      <style jsx="true">
+        {`
+          .FormFilters__top {
+            padding: 4rem 0;
+          }
+          .FormFilters__bottom {
+            display: none;
+          }
+          @media screen and (max-width: 991.98px) {
+            .FormFilters__top {
+              display: none;
+            }
+            .FormFilters__bottom {
+              display: block;
+            }
+          }
+        `}
+      </style>
     </div>
   );
 };
