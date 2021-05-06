896ff6ba040c439d9f504189b6b409258435c151
truongducthang <ducthangm9@gmail.com>
2021-05-06 15:13:29 +0700

    del

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import Banner from '../../components/HomePageComponents/Banner/Banner';
 import CarouselCpn from '../../components/HomePageComponents/Carousel/Carousel';
 import './_Home.scss';
-import Products from '../../components/HomePageComponents/Products';
+// import Products from '../../components/HomePageComponents/Products';
 import Destinations from '../../components/HomePageComponents/Destinations';
 import Promotions from '../../components/HomePageComponents/Promotions';
 const HomePage = () => {
